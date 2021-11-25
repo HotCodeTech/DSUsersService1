11e73edf96935d4c55aa5ad2a1528682970ff44c
Felix Wafula <felix.a.wafula@gmail.com>
2021-11-25 07:06:54 -0700

    removed connection strings

--- UsersService.Api/Startup.cs
+++ UsersService.Api/Startup.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7d89007f40f9a8d5fcc67bf20a5b8215193733d45545aef376364e91eaf1f9ea
-size 2370
+oid sha256:9a40c86ad3e76618ec8f5363efab15a70f2edebd16167c96989eb5f38b40ad98
+size 2190
